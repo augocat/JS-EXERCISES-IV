cf30574f2ac0c2246feef3987818529b4f6b71d7
narb <narborough.grahame@icloud.com>
2024-09-05 14:11:01 +1000

    add FILM_FINDER to 14.REQUESTS WITH FETCH

---
+++ 14.REQUESTS WITH FETCH API/FILM_FINDER/public/script.js
@@ -0,0 +1,29 @@
+//FILM_FINDER
+const tmdbKey = '';
+const tmdbBaseUrl = '';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = () => {
+
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
